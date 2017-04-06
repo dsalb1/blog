6a1b8a793fa111483e831357cb12126d5136778e
Dan Salberg <daniel.j.salberg@gmail.com>
2017-04-06 17:54:06 -0500

    working copy

--- main.py
+++ main.py
@@ -1,25 +1,70 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import jinja2
 import webapp2
+import os
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+	def get(self, *a, **kw):
+		self.response.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.response.write(self.render_str(template, **kw))
+
+class BlogPost(db.Model):
+	title = db.StringProperty(required = True)
+	blogpost = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+
+
+class MainBlog(Handler):
+	def get(self):
+		#self.render("blog.html")
+		posts = db.GqlQuery("SELECT * FROM BlogPost ORDER BY created DESC LIMIT 5")
+		
+		self.render('blog.html', posts=posts)
+
+	"""def post(self):
+		title = self.request.get("title")
+		blogpost = self.request.get("blogpost")
+
+		if title and blogpost:
+			self.redirect()
+		else:
+			error="You need to fill out both fields to post a new entry"
+			self.render("base.html", error=error)"""
+
+class NewPost(Handler):
+	def get(self):
+		self.render("newpost.html")
+
+	def post(self):
+		title = self.request.get("title")
+		blogpost = self.request.get("blogpost")
+
+		if title and blogpost:
+			p = BlogPost(title=title, blogpost=blogpost)
+			p.put()
+
+			self.redirect('/blog')
+		else:
+			error="You need to fill out both fields to post a new entry"
+			self.render("newpost.html", title=title, blogpost=blogpost, error=error)
+
+class ViewPostHandler(webapp2.RequestHandler):
+	def get(self, id):
+		id = int(id)
+		return BlogPost.get_by_id(id)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+	('/blog', MainBlog),
+	('/newpost', NewPost),
+	webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
 ], debug=True)
